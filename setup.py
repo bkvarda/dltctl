3a065c5f892f94b39abf334970db0ba92753eac7
Brandon Kvarda <bjkvarda@gmail.com>
2022-10-18 08:42:56 -0700

    Added versioning for releases

--- setup.py
+++ setup.py
@@ -4,14 +4,26 @@ from setuptools import setup, find_packages
 if sys.version_info < (3,5):
     sys.exit('Python < 3.5 is not supported')
 
+def get_version():
+    version = None
+    with open('dltctl/version.py', 'r') as f:
+        val = (f.readline()
+        .split('=')[1]
+        .strip()
+        .strip('/"'))
+        version = val
+    return version
+
+
+__version__ = get_version()
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
-version = "0.1"
 setup(
     name='dltctl',
-    version=version,
+    version=__version__,
     packages=find_packages(include=['dltctl*']),
     install_requires=[
         'databricks-cli>=0.17.3',
